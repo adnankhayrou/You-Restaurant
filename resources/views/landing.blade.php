88f12524dfbcf63507a243b988638bb4a9e0789b
adnankhayrou <adnanekhayrou3@gmail.com>
2023-02-06 16:30:16 +0100

    adding landing page and dashboard

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -47,49 +47,18 @@
                 </div>
               </nav>
 
-              <div class="container row">
-                <div class="col-3 m-4 card p-0" style="width: 18rem;">
-                    <img src="{{asset('images/85305.jpg')}}" class="card-img-top">
+              <div class="container row mx-auto">
+                @for($i=0;$i<9;$i++)
+                <div class="col-lg-3 p-3 col-12">
+                    <div class=" card">
+                        <img src="{{asset('images/85305.jpg')}}" class="card-img-top">
                     <div class="card-body">
                       <h5 class="card-title">Card title</h5>
                       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     </div>
                   </div>
-                  <div class="col-3 m-4 card p-0" style="width: 18rem;">
-                    <img src="{{asset('images/265693.png')}}" class="card-img-top">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                  </div>
-                  <div class="col-3 m-4 card p-0" style="width: 18rem;">
-                    <img src="{{asset('images/82263.jpg')}}" class="card-img-top">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                  </div>
-                  <div class="col-3 m-4 card p-0" style="width: 18rem;">
-                    <img src="{{asset('images/521718.jpg')}}" class="card-img-top">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                  </div>
-                  <div class="col-3 m-4 card p-0" style="width: 18rem;">
-                    <img src="{{asset('images/85305.jpg')}}" class="card-img-top">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                  </div>
-                  <div class="col-3 m-4 card p-0" style="width: 18rem;">
-                    <img src="{{asset('images/265693.png')}}" class="card-img-top">
-                    <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                  </div>
+                </div>
+                @endfor
                   
               </div>
     </body>
